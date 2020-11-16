f1c8a97e550a9328a13f46072cfed27a2df7c8cf
Marlesss <60238634+Marlesss@users.noreply.github.com>
2020-11-16 16:36:13 +0300

    Update program.py

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ def main():
     print("My first git program")
     print("My second commit")
     print("And now im gonna turn around")
+    print("somebody once told me")
 
 
 if __name__ == "__main__":
